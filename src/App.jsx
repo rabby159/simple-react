8393f66551a88f250f60d6ff85134e65189c1885
rabby159 <ablazerabbi159@gmail.com>
2025-03-10 02:15:50 +0600

    rendering use map

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Map from "./map";
 import Todo from "./Todo";
 
 function App() {
+  // const bikes = ["Suzuki", "Yamaha", "Hero", "Honda"];
+  const riders = [
+    { id: 1, name: "Rabby", age: 25 },
+    { id: 2, name: "Bijoy", age: 26 },
+    { id: 3, name: "Roni", age: 35 },
+  ];
+
   return (
     <>
       <h1>Vite + React</h1>
@@ -12,6 +20,14 @@ function App() {
       <Todo topic="Learn Node" isCompleted={false}></Todo>
       <Todo topic="Learn MongoDB" isCompleted={false}></Todo>
       <Todo topic="Learn Redux" isCompleted={true}></Todo>
+      // dynamic map
+      {/* {bikes.map((bike) => (
+        <Map bike={bike}></Map>
+      ))} */}
+
+      {riders.map((rider) => (
+        <Map rider={rider}></Map>
+      ))}
     </>
   );
 }
---
+++ src/map.jsx
@@ -0,0 +1,19 @@
+// export default function Map({ bike }) {
+//   return (
+//     <div>
+//       <p>Name: {bike}</p>
+//     </div>
+//   );
+// }
+
+// ---------------------------------------------------
+
+export default function Map({ rider }) {
+    return (
+      <div>
+        <p>Name: {rider.name}</p>
+        <p>age: {rider.age}</p>
+      </div>
+    );
+  }
+
