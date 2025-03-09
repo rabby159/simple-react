e4879803ca9a0664de81f3b67ee373e0fe6ef056
rabby159 <ablazerabbi159@gmail.com>
2025-03-10 01:35:51 +0600

    conditional rendering

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,8 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-export default function Todo({topic, isCompleted}) {
+// export default function Todo({topic, isCompleted}) {
+//     return (
+//         <div>
+//             <li>Task: {topic}</li>
+//         </div>
+//     )
+// }
+
+
+// conditional rendering
+export default function Todo({ topic, isCompleted }) {
+  if (isCompleted) {
     return (
-        <div>
-            <li>Task: {topic}</li>
-        </div>
-    )
+      <div>
+        <li>Complete: {topic}</li>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <li>Still Working: {topic}</li>
+      </div>
+    );
+  }
 }
